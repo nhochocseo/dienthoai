06d44fba18f704a9d99e7d86c53e6e9cf6a0cc3d
nhochocseo <caodung9x@gmail.com>
2016-04-20 12:50:54 +0700

    lần 3
    abv

--- content.php
+++ content.php
@@ -21,9 +21,8 @@
 			</li>
 		</ul>
 	</div>
-<div class="module_menu">
-	<div class="module-content clearfix">
-		<ul class="accordion-menu" id="demo-menu">
+	<div class="module-content">
+		<ul class="accordion-menu">
 			<li class="item-wrapper">
 				<a href="/category1">Category 1</a>	
 			</li>
@@ -34,14 +33,10 @@
 				<div class="ul-wrapper">
 					<ul>
 						<li>
-							<div class="item-wrapper">
-								<a href="/category2/2013-01-16-23-09-22">Sub category 1</a>
-							</div>
+							<a href="/category2">Sub 1 in category 2</a>
 						</li>
 						<li>
-							<div class="item-wrapper">
-								<a href="/category2/2013-01-16-23-09-23">Sub category 2</a>
-							</div>
+							<a href="/category2">Sub 2 in category 2</a>
 						</li>
 					</ul>
 				</div>
@@ -53,23 +48,18 @@
 				<div class="ul-wrapper">
 					<ul>
 						<li>
-							<div class="item-wrapper">
-								<a href="/category3/2013-01-16-23-09-22">Sub category 1</a>	
-							</div>
+							<a href="/category3">Sub 1 in category 3</a>
 						</li>
 						<li>
-							<div class="item-wrapper">
-								<a href="/category3/2013-01-16-23-09-23">Sub category 2</a>
-							</div>
+							<a href="/category3">Sub 2 in category 3</a>
 						</li>
 					</ul>
 				</div>
 			</li>
 			<li class="item-wrapper">
-					<a href="/category4">Category 4</a>
+				<a href="/category4">Category 4</a>
 			</li>
 		</ul>
 	</div>
-</div>
 	</div><!-- End Coll 3 -->
 </div>
